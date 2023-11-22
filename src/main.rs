99466e7e0ef91e36f811558da9a05df468a299ba
breayhing <1519981563@qq.com>
2023-11-22 20:04:47 +0800

    feat: implement insertion and basic in_order_travel in bst

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,68 @@
-use std::io;
-use study::acronym;
+use std::cell::{Ref, RefCell};
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct TreeNode {
+    value: i32,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    fn new(value: i32) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(TreeNode {
+            value,
+            left: None,
+            right: None,
+        }))
+    }
+}
+
+fn in_order_traversal(node:&Option<Rc<RefCell<TreeNode>>>){
+    if let Some(node) =node{
+        in_order_traversal(&node.borrow().left);
+        println!("{}",node.borrow().value);
+        in_order_traversal(&node.borrow().right);
+    }
+}
+
+fn insert_into_bst(root: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    if let Some(root) = root {
+        let mut root_borrowed = root.borrow_mut();
+        if value < root_borrowed.value {
+            root_borrowed.left = insert_into_bst(&root_borrowed.left, value);
+        } else if value > root_borrowed.value {
+            root_borrowed.right = insert_into_bst(&root_borrowed.right, value);
+        }
+        Some(root.clone())
+    } else {
+        // create new root and return if no tree exists
+        Some(TreeNode::new(value))
+    }
+}
 
 fn main() {
-    println!("Enter a long name to generate its acronym:");
+    let root = TreeNode::new(1);
+}
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    let acronym = acronym(&input.trim());
+#[cfg(test)]
+mod tests{
+    use super::*;
 
-    println!("Long name: {}",input.trim());
-    println!("Acronym:  {}",acronym);
+    #[test]
+    fn test_tree_creation(){
+
+    }
+
+    #[test]
+    fn test_tree_modification(){
+
+    }
+
+    #[test]
+    fn test_in_order_traversal(){
+
+    }
+    
 }
